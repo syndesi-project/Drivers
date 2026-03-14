723748e9064f66ecda3c59c2a87363b373fa17ac
Sébastien Deriaz <sebastien.deriaz1@gmail.com>
2026-03-14 09:35:41 +0100

    Remove C++ and C#, new folder organization

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# drivers
-Syndesi device drivers
+# Syndesi drivers Python package
+
+This package contains all the official Syndesi Drivers
+
+## Driver structure
+
+```python
+class DeviceName(Interfaces):
+    def __init__(self, adapter : IAdapter) -> None:
+        """
+        Device description
+        """
+        super().__init__()
+
+        # Check the adapter type here
+        assert isinstance(adapter, ...), "Invalid adapter"
+
+        # Use a protocol
+        self._prot = SCPI(adapter)
+        #self._prot = Raw(adapter)
+
+    # Declare a method
+    def my_method(self) -> int:
+        """
+        Method description
+        """
+        self._prot.write('...')
+        data = self._prot.read('...')
+        return int(data)
+```
--- syndesi_drivers/test_and_measurement/multimeters/siglent_sdm30x5.py
+++ syndesi_drivers/test_and_measurement/multimeters/siglent_sdm30x5.py
@@ -1,11 +1,67 @@
 from .multimeters import Multimeter
-from ..scpi_driver import SCPIDriver
-from syndesi.adapters import Adapter, IP, VISA
-from syndesi.protocols import SCPI
+from syndesi import Adapter, IP, VISA, SCPI
 from enum import Enum
 from typing import Union
 
 # https://int.siglent.com/upload_file/user/SDM3055/SDM3055_RemoteManual_RC06035-E01A.pdf
+
+# https://int.siglent.com/upload_file/user/SDM3055/SDM3055_RemoteManual_RC06035-E01A.pdf
+
+class SDM3055(IVoltmeter, IAmmeter):
+    def __init__(self, adapter : IAdapter) -> None:
+        """
+        Siglent SDM3055 5½ digit multimeter
+
+        Parameters
+        ----------
+        adpater : IAdapter
+            Adapter to use, both IP and VISA are allowed
+        """
+        super().__init__()
+
+        assert isinstance(adapter, IP) or isinstance(adapter, VISA), "Invalid adapter"
+        self._prot = SCPI(adapter)
+
+    def measure_ac_current(self) -> float:
+        """
+        Make an AC current measurement and return the result
+        """
+        self._prot.write('CONF:CURR:AC')
+        self._prot.write('INIT')
+        self._prot.write('*TRG')
+        output = float(self._prot.query('FETC?'))
+        return output
+
+    def measure_dc_current(self) -> float:
+        """
+        Make a DC current measurement and return the result
+        """
+        self._prot.write('CONF:CURR:DC')
+        self._prot.write('INIT')
+        self._prot.write('*TRG')
+        output = float(self._prot.query('FETC?'))
+        return output
+
+    def measure_ac_voltage(self) -> float:
+        """
+        Make an AC voltage measurement and return the result
+        """
+        self._prot.write('CONF:VOLT:AC')
+        self._prot.write('INIT')
+        self._prot.write('*TRG')
+        output = float(self._prot.query('FETC?'))
+        return output
+
+    def measure_dc_voltage(self) -> float:
+        """
+        Make a DC voltage measurement and return the result
+        """
+        self._prot.write('CONF:VOLT:DC')
+        self._prot.write('INIT')
+        self._prot.write('*TRG')
+        output = float(self._prot.query('FETC?'))
+        return output
+
 
 __RANGE_TOLERANCE = 0.01 # 1%
 
